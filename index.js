a40dc0a36de77825cfa6748f4ab72e5a2ed48b8a
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-12-02 15:06:44 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+// document.getElementById('heading').innerHTML ("<strong>")
+ const head = document.createElement('h1');
+ head.innerHTML= 'My HTML adventure';
+ document.body.appendChild(head);
+
+ let p = document.createElement('p');
+ p.innerHTML = '<strong><em><a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><table></table></a></em></strong>';
+//  p.innerHTML = '<em></em>';
+ document.body.appendChild(p);
+
+
+
+
+
+//  let p1 = document.createElement('a');
+//  p.innerHTML = '<strong><em></em></strong>';
+//  p.innerHTML = '<em></em>';
+//  document.body.appendChild(p1);
+
+
+
+//  let p1 = document.createElement('em');
+// //  p.innerHTML = '<strong></strong>';
+//  p1.innerHTML = '<em></em>';
+//  document.body.appendChild(p1);
+
+//  let p = document.createElement('p');
+//  p.innerHTML = '<em></em>';
+//  document.body.appendChild(p);
+
+
+
+//  p.innerHTML = '<em></em>';
+ 
+
+//  let p = document.createElement
+ 
+//  let strong = par.append('strong');
+// window.document.appendChild(par);
+//  par =document.createElement('strong');
+//  document.body.appendChild(par);
+
+// par = document.querySelector('strong');
+// document.body.appendChild('strong');
+
+// const p1 = document.createElement('strong');
+//  head.innerHTML= 'strong';
+//  Element.appendChild(p1);
